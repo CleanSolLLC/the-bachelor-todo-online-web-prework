f4eb45ffd981950bc4e48756aa60be83ee44dcb7
Mark May <mamark90@gmail.com>
2019-05-24 12:26:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,16 +1,17 @@
 require 'pry'
 
 def get_first_name_of_season_winner(data, season)
-  data.each do |k,v| #hash with seasons as keys
-      v.each do |a|
-        a.each do |k1,v1|
+  data.each do |season_num,season_data| #hash with seasons as keys values all contestant data
+    if season_num == season
+      season_data.each do |contestant_info|  # itereate over contestant data stored in arrays
+        contestant_info.each do |attributes,data| #iterate over arrays to retrieve hash keys and values
           binding.pry
       end
     end
   end
 end
 
-    #if season_num == season
+    
 
 
 def get_contestant_name(data, occupation)
